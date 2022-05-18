64620ff8abaa9c6bf4e8d8770011f766fc387893
owaisklabs <owais.klabs@gmail.com>
2022-05-18 22:12:49 +0500

    fixes

---
+++ app/Http/Controllers/OfferController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\ProductBid;
+use App\ProductSize;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OfferController extends Controller
+{
+    public function offers(){
+        $bids = ProductBid::where(function ($query) {
+            $query->where('user_id', Auth::user()->id)->where('counter', NULL);
+        })->orwhere(function ($query) {
+            $query->where('vendor_id', Auth::id())
+                ->where('counter', '!=', NULL);
+        })->get();
+
+        foreach ($bids as $bid) {
+            $product = Product::select('id','user_id','name','condition','available','size_id')->where('id', $bid->product_id)
+                ->where('available',1)
+                ->first();
+            $size = ProductSize::select('id','text')->find($product->size_id);
+            $bid->setAttribute('product', $product);
+            $bid->setAttribute('size', $size);
+        }
+
+        //         $bids1 = ProductBid::where('user_id', Auth::user()->id)->get();
+        //   foreach($bids1 as $bid){
+        //                 $product=Product::with('images')->with('user')->where('id',$bid->product_id)->first();
+        //                 $bid->setAttribute('product',$product);
+        //             }
+
+        //         $bids2 = ProductBid::where('vendor_id', Auth::user()->id)->where('counter','!=',NULL)->get();
+        //   foreach($bids2 as $bid){
+        //                 $product=Product::with('images')->with('user')->where('id',$bid->product_id)->first();
+        //                 $bid->setAttribute('product',$product);
+        //             }
+        //             array_push($bids1,$bids2);
+        if ($bids) {
+            $status = 'True';
+            $message = 'You Complete Offers...';
+            return response()->json(compact('status', 'message', 'bids'), 201);
+        } else {
+            $status = 'False';
+            $message = 'You Don`t Send any Offers';
+            return response()->json(compact('status', 'message'), 201);
+        }
+    }
+}
