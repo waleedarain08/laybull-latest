f145a387dc7aebae2f442ba6a4ef2eed2199afa2
owaisklabs <owais.klabs@gmail.com>
2022-05-20 17:38:19 +0500

    fixes

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Notification;
 use App\Product;
 use App\ProductBid;
 use App\ProductSize;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class OfferController extends Controller
 {
@@ -19,7 +21,7 @@ class OfferController extends Controller
         })->get();
 
         foreach ($bids as $bid) {
-            $product = Product::select('id','user_id','name','condition','sold','size_id')->where('id', $bid->product_id)
+            $product = Product::select('id','user_id','name','condition','featured_image','sold','size_id')->where('id', $bid->product_id)
                 ->where('available',1)
                 ->first();
             $size = ProductSize::select('id','text')->find($product->size_id);
@@ -77,6 +79,32 @@ class OfferController extends Controller
         } else {
             $status = 'False';
             $message = 'You Dont Have Any Offers';
+            return response()->json(compact('status', 'message'), 201);
+        }
+    }
+    public function bid_counter(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'bid_id' => 'required',
+            'counter' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->formatResponse('error','validation error',$validator->errors()->first(),401);
+        }
+
+        $bid = ProductBid::findOrFail($request->bid_id);
+        $bid->counter = $request->counter;
+
+        $bid->update();
+
+        if ($bid) {
+            $status = 'True';
+            $message = 'Product Biding Counter SuccessFully...';
+            return response()->json(compact('status', 'message', 'bid', 'notification'), 201);
+        } else {
+            $status = 'False';
+            $message = 'Something Went Wrong';
             return response()->json(compact('status', 'message'), 201);
         }
     }
