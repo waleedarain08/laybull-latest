a14cfbbc340d063f08c3673fced874a293d22321
owaisklabs <owais.klabs@gmail.com>
2022-07-04 16:11:46 +0500

    fuxes

---
+++ app/Http/Controllers/DiscountController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Discount;
+use Illuminate\Http\Request;
+
+class DiscountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $discounts = Discount::all();
+        return view('admin.discount.index',get_defined_vars());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.discount.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+//        return $request->all();
+        $discount = new Discount();
+        $discount->coupon_name = $request->coupon_name;
+        $discount->discount_percentage = $request->discount_percentage;
+        $discount->save();
+        return redirect()->route('discount.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Discount $discount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Discount $discount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Discount $discount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Discount $discount)
+    {
+        $discount->delete();
+        return  redirect()->route('discount.index');
+    }
+}
